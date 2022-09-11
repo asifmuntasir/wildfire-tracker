928f7522e1f337f0eb3728c4bd7c3d02c80055be
asif-96 <shuyeb.dk@gmail.com>
2022-09-12 00:41:07 +0600

    add current location

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -2,27 +2,40 @@ import React from 'react';
 import {
     MapContainer,
     TileLayer,
-    useMap,
+    Marker,
+    Popup,
 } from 'react-leaflet';
-import LeafletCanvasMarker from './LeafletCanvasMarker';
+import LocationMarker from './LocationMarker';
+import { Icon } from '@iconify/react'
+import locationIcon from '@iconify/icons-mdi/fire-alert'
+
+const center = {
+    lat: 24.9082,
+    lng: 91.8678,
+}
 
 const LeafletMap = () => {
     return (
         <div className='map'>
-            <MapContainer style={{ height: '100%' }} center={[24.894802, 91.869034]} zoom={13} scrollWheelZoom={false}>
+            <MapContainer style={{ height: '100%' }} center={center} zoom={13} scrollWheelZoom={false}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                {/* <Marker position={[51.505, -0.09]}>
+                {/* <LeafletCanvasMarker /> */}
+                {/* <LocationMarker lat={center.lat} lng={center.lng} /> */}
+                <Marker position={center}>
                     <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
+                        Amberkhana, Sylhet.
                     </Popup>
-                </Marker> */}
-                <LeafletCanvasMarker />
+                </Marker>
+                {/* <div className='location_marker'>
+                    <Icon center={center} icon={locationIcon} className='location-icon' />
+                </div> */}
             </MapContainer>
         </div>
     );
 };
+
 
 export default LeafletMap;
