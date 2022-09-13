e2e2c59069d5ed24b26f9b967d1b34cfaa4af2e0
asifmuntasir <shuyeb.dk@gmail.com>
2022-09-13 21:32:34 +0600

    load data from nasa govt api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
+import { useState, useEffect } from "react";
 import LeafletMap from "./components/LeafletMap";
 
 function App() {
+
+  const [eventData, setEventData] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true)
+      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
+      const { events } = await res.json()
+
+      setEventData(events)
+      setLoading(false)
+    }
+
+    fetchEvents();
+
+    // console.log(eventData);
+  }, []);
+
   return (
     <>
       <h1>Hello...Wild Life</h1>
